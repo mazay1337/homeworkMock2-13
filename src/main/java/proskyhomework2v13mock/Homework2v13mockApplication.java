8acc5bcd2eed302ebf352966d0a82f9b3451e50d
Танин <mazayztn@yandex.ru>
2024-02-11 19:47:53 +0300

    initial commit

---
+++ src/main/java/proskyhomework2v13mock/Homework2v13mockApplication.java
@@ -0,0 +1,13 @@
+package proskyhomework2v13mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework2v13mockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework2v13mockApplication.class, args);
+	}
+
+}
